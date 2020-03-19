accbd01a150b0e95ad2c6b35b99708b5634a8e78
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-19 19:58:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,14 @@ class Triangle
   
   def initialize(a,b,c)
     
+    if a == b && b == c 
+      "equilteral"
+      elsif a == b && b != c 
+      "isosceles"
+    else
+      "scalene"
+      
+      
     
   end
   
