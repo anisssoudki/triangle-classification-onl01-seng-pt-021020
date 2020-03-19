1c5989157c4e8f90d9adfb588832e79efdd7145b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-19 20:13:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,36 @@
 class Triangle
-  attr_reader :a, :b, :c
-  def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
   end
 
   def kind
-    validate_triangle #runs this method
-    #returns what kind of triangle it is
-    if a == b && b == c
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
       :equilateral
-    elsif a == b || b == c || a == c
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
       :isosceles
-    else
-      :scalene
-    end
+    end 
   end
-
-  def validate_triangle
-    #creates an array of boolean values basedon the input values of a,b,c ; so that if any of those return false, the method will return false, if the array has a false value in it, then the raise Triangle Error runs
-    #otherwise this returns true
-    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-    #below tests to see if a,b or c is zero or negative
-    #if any of them are zero or negative, then it adds a false value to the array, so that the Triangle Error will be raised
-    [a, b, c].each { |s| real_triangle << false if s <= 0 }
-    raise TriangleError if real_triangle.include?(false)
-  end
+  
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
 end
 
 class TriangleError < StandardError
+  
 end
