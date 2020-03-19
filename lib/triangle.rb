9641bcb6c55072cdeaac63824120d86eba18ce39
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-19 20:11:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,9 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
+      elsif
+      (@side_1 = -1) || (@side_2 = -1) || (@side_3 = -1)
+      raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
